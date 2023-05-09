8ab33de97a4ff597e7f83f919fc4a800e6ddeb0e
blaje <blaje@abv.bg>
2023-05-09 15:48:26 +0300

    React music album rotator

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import AlbumRotation from './components/AlbumRotation';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      {/* <div className='main'>
+        <div className='gradient' />
+      </div> */}
+
+      <div className='app'>
+        <AlbumRotation />
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/AlbumRotation.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./AlbumRotation.css";
+
+const initialList = ['A', 'B', 'C', 'D', 'E'];
+
+const debounce = (func, delay) => {
+    let timer;
+    return function (...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
+};
+
+const AlbumRotation = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [list, setList] = useState(initialList);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setList(prevList => {
+                const prevListWithoutTheLastEl = prevList.slice(0, -1);
+                const fifth = prevList[4];
+                if (prevListWithoutTheLastEl.length >= 5) {
+                    prevListWithoutTheLastEl.splice(4, 1);
+                }
+                const last = prevList.slice(-1)[0];
+                return [last, ...prevListWithoutTheLastEl];
+            });
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, [searchTerm]);
+
+    useEffect(() => {
+        const debouncedSearch = debounce(() => {
+            if (searchTerm) {
+                axios
+                    .get(`https://itunes.apple.com/search?term=${searchTerm}&entity=album`,{
+                        headers: {
+                            "Access-Control-Allow-Origin": "*"
+                          }
+                    })
+                    .then(async (response) => {
+                        const albumNames = await response.data.results
+                            .map((result) => result.collectionName)
+                            .sort()
+                            .slice(0, 5);
+                        setList((prevAlbum) => {
+                            const [one, two, three, four, five, ...rest] = prevAlbum;
+                            const newAlbum = [one, two, three, four, five, ...albumNames];
+
+                            console.log(albumNames, prevAlbum, newAlbum);
+
+                            return newAlbum;
+                        });
+                    })
+                    .catch((error) => console.error(error));
+            }
+        }, 1000);
+        debouncedSearch();
+    }, [searchTerm]);
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    return (
+        <div className="search-container">
+            <input type="text"
+                placeholder="Search"
+                value={searchTerm}
+                onChange={handleSearch} />
+            <ul>
+                {list.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default AlbumRotation;
